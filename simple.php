eae4c1be6d67361728c828c4e99afc0882c7a7b5
Amara <54652581+amaraauguste@users.noreply.github.com>
2020-07-15 08:09:30 -0400

    Update simple.php

--- simple.php
+++ simple.php
@@ -1,8 +1,1 @@
-<html>
- <head>
-  <title>Dynamic Web Page</title>
- </head>
- <body>
- <?php $n = rand(1, 9999) * 1414; echo ("Random value is " . $n); ?> 
- </body>
-</html>
+ <?php $n = rand(1, 9999) * 1203; echo ("Random value is " . $n); ?> 
